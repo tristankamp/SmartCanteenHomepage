d69c593c8fbda42cfc2f46e022ee1b92b61a2162
RasmusRB <57805908+RasmusRB@users.noreply.github.com>
2020-11-26 10:35:56 +0100

    https://github.com/tristankamp/SmartCanteenHomepage

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,36 @@
+import axios, {
+    AxiosError,
+    AxiosResponse
+} from "../../node_modules/axios/index"
+
+interface ICustomer{
+    counter: number
+    dateTime: Date
+}
+
+let baseUrl: string = "//TODO rest uri";
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+      customerdata: [],
+      idToGetBy: -1
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        helperGetAndShow(url: string){ // helper method 
+            axios.get<ICustomer[]>(url)
+            .then((response: AxiosResponse<ICustomer[]>) => {
+                this.customerdata = response.data
+            })
+            .catch((error: AxiosError) => {
+                alert(error.message)
+            })
+        }, 
+        getAllData(){
+            this.helperGetAndShow(baseUrl)
         }
     }
 })
